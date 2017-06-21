13d98cc229627265883ba3e6ee34f93364b5f6c8
suhailtajshaik <suhailtajshaik@gmail.com>
2017-06-21 01:32:22 -0500

    slack bot Authentication

--- bin/run.js
+++ bin/run.js
@@ -4,13 +4,17 @@ const slackClient = require('../server/slackClient');
 const service = require('../server/service');
 const http = require('http');
 const server = http.createServer(service);
+const bot_port = process.env.SLACK_BOT_PORT || 3000;
 const bot_token = process.env.SLACK_BOT_TOKEN || 'xoxb-201789800167-c0iu7WgiDxMwSHIxnoeIWJg1';
-const bot_logLevel = 'debug';
+const bot_logLevel = 'verbose';
 
 const rtm = slackClient.init(bot_token, bot_logLevel);
 rtm.start();
-server.listen(3000);
+
+slackClient.addAuthenticatedHandler(rtm, () => server.listen(bot_port));
+
 
 server.on('listening', function () {
     console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode`);
 })
+
--- server/slackClient.js
+++ server/slackClient.js
@@ -2,9 +2,33 @@
 
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+let rtm = null;
+let channel;
+
+const handleOnAuthenticated = (rtmStartData) => {
+    // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
+    for (const c of rtmStartData.channels) {
+        if (c.is_member && c.name === 'general') { channel = c.id }
+    }
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+    onConnected();
+}
+
+const addAuthenticatedHandler = (rtm, handler) => {
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
+}
+
+// Send responce to slack on connected
+const onConnected = () => {
+    rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+        rtm.sendMessage("Hello!", channel);
+    });
+}
 
 module.exports.init = function slackClient(bot_token, logLevel) {
-    const rtm = new RtmClient(bot_token, { logLevel: logLevel });
+    rtm = new RtmClient(bot_token, { logLevel: logLevel });
+    addAuthenticatedHandler(rtm, handleOnAuthenticated);
     return rtm;
 }
 
+module.exports.addAuthenticatedHandler = addAuthenticatedHandler;
