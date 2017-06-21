5a5b50b6e0d423e3c185f4319ac4d5f02e9108d4
suhailtajshaik <suhailtajshaik@gmail.com>
2017-06-21 01:39:48 -0500

    slack bot Authentication

--- server/slackClient.js
+++ server/slackClient.js
@@ -2,6 +2,7 @@
 
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 let rtm = null;
 let channel;
 
@@ -11,7 +12,6 @@ const handleOnAuthenticated = (rtmStartData) => {
         if (c.is_member && c.name === 'general') { channel = c.id }
     }
     console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
-    onConnected();
 }
 
 const addAuthenticatedHandler = (rtm, handler) => {
@@ -19,15 +19,20 @@ const addAuthenticatedHandler = (rtm, handler) => {
 }
 
 // Send responce to slack on connected
-const onConnected = () => {
-    rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+const handleOnConnected = () => {
         rtm.sendMessage("Hello!", channel);
-    });
+}
+
+// Handle on message
+const handleOnMessage = (message) => {
+
 }
 
 module.exports.init = function slackClient(bot_token, logLevel) {
     rtm = new RtmClient(bot_token, { logLevel: logLevel });
     addAuthenticatedHandler(rtm, handleOnAuthenticated);
+    // handleOnConnected();
+    rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, handleOnConnected)
     return rtm;
 }
 
